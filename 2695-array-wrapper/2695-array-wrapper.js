7864717f1daa301c1095d75fc3f2e588f83860be
Deepanshu Heer <deepanshuheer27@gmail.com>
2024-08-07 14:04:08 +0530

    Time: 61 ms (16.90%), Space: 49.1 MB (94.06%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    const arr = this.nums;
+    let sum = 0;
+    for(let i = 0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.nums)
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
